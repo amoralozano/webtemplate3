5498412e57ced54a051338c77c2276c36e6b162c
abdael mora <azmoracrads@gmail.com>
2024-02-09 10:53:18 -0700

    added login and 2nd landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import alpinejs from "@astrojs/alpinejs";
 import icon from "astro-icon";
 
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(), icon(), tailwind()]
+  integrations: [alpinejs(), icon(), tailwind()],
 });
